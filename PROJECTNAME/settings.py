33ce36118cbd4d04b3d21d56f0c31a84666afd92
H4R5H <151930114+g0j0-H4R5H@users.noreply.github.com>
2023-12-29 19:22:13 +0530

    Update settings.py
    for security

--- PROJECTNAME/settings.py
+++ PROJECTNAME/settings.py
@@ -132,7 +132,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "harshgupta84127@gmail.com"
-EMAIL_HOST_PASSWORD = "lwyz ibjz mamz guil"
+EMAIL_HOST_USER = "mygmail@gmail.com"
+EMAIL_HOST_PASSWORD = "my_app_password"
 
 KEY = config('KEY')
